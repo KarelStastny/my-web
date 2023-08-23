0dfffc08e8f499ad591bccae6d622034404155fa
Karel Šťastný <karell29@icloud.com>
2023-08-23 14:02:14 +0200

    upraven banner s logama a experinece

--- src/Front-end/components/Banner.jsx
+++ src/Front-end/components/Banner.jsx
@@ -4,10 +4,10 @@ import iconBanerData from "../../Back-end/iconBaner";
 const Banner = () => {
   return (
     <div className="flex items-center justify-center 
-    h-[150px] p-2 md:p-4">
+    lg:h-[150px] p-4 lg:p-6 gap-4   bg-dark">
       {iconBanerData.map((one) => {
-        return <div key={one.id} className="flex items-center justify-center h-full">
-          <img className="object-cover h-full" src={one.image} alt="" />
+        return <div key={one.id} className="flex  Md:p-2 items-center justify-center h-full w-full">
+          <img className="object-contain h-full w-full" src={one.image} alt="" />
         </div>;
       })}
     </div>
--- src/Front-end/components/Experience.jsx
+++ src/Front-end/components/Experience.jsx
@@ -14,21 +14,21 @@ const Experience = () => {
   };
 
   return (
-    <div className="faq">
-      <div className="w-[350px] bg-dark">
+    <div className="faq p-4">
+      <div className="w-full max-w-[500px] m-auto ">
         {dataExperience.map((one) => (
           <div className="itembox" key={one.id}>
             <div
-              className="title flex justify-between items-center p-4 rounded bg-dark-brown cursor-pointer transition-colors hover:text-orange-icon"
+              className=      "flex justify-between items-center p-2 bg-dark rounded-md mb-2 cursor-pointer hover:text-purple-500 font-semibold pl-4 transition-all duration-150"
               onClick={() => toggle(one.id)}
             >
-              <h2 className="text-white-brown">{one.heading}</h2>
-              <span className="text-2xl font-bold">{selected === one.id ? <MdKeyboardArrowUp/> : <MdKeyboardArrowDown/>}</span>
-            </div>
+              <h2 className={` ${selected === one.id ? ' text-purple-500' : 'text-white' } `}>{one.heading}</h2>
+              <span className="text-2xl font-bold">{selected === one.id ? <MdKeyboardArrowUp className='fill-purple-500' /> : <MdKeyboardArrowDown className='fill-purple-500'/>}</span>
+            </div> 
             <div
-              className={`content ${selected === one.id ? 'max-h-500px transition-max-height duration-2000' : 'max-h-0 overflow-hidden transition-max-height duration-700'}`}
+              className={` ${selected === one.id ? 'max-h-500px transition-max-height duration-2000' : 'max-h-0 overflow-hidden transition-max-height duration-700'}`}
             >
-              <p className="p-4 text-justify">{one.text}</p>
+              <p className=" px-4 pb-2 text-justify">{one.text}</p>
             </div>
           </div>
         ))}
