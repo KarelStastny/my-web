44d9eb4b6191a1982ff13d7149e28cbab91495b5
Karel Šťastný <karell29@icloud.com>
2023-09-25 07:53:09 +0200

    doplěny informace

--- src/Back-end/dataExperience.js
+++ src/Back-end/dataExperience.js
@@ -1,53 +1,55 @@
 const dataExperience = [
-    {
-      id: 1,
-      heading: 'HTML',
-      text: 'Základní stavební kámen webu. Umím vytvářet strukturu obsahu, pracovat s tagy a elementy a zajistit, aby byla stránka dobře strukturovaná a přístupná.',
-    },
-    {
-      id: 2,
-      heading: 'CSS',
-      text: 'Kouzlo vizuálního vzhledu stránky. Nebojím se experimentovat s různými styly, od responzivního flexboxu až po CSS Grid, ale mám slabost pro flexbox.',
-    },
-    {
-      id: 3,
-      heading: 'SCSS',
-      text: 'SCSS mi umožňuje psát čistý a strukturovaný kód díky možnosti dědičnosti a mixinů. Rád využívám jeho sílu k vytvoření efektivních a dobře organizovaných stylů, a také se s ním lépe orientuji v rozsáhlejších projektech.',
-    },
-    {
-      id: 4,
-      heading: 'Tailwind CSS',
-      text: 'Mým nejoblíbenějším stylem je Tailwind CSS. Jeho jedinečné třídy mi umožňují rychle a elegantně implementovat designové prvky a rychle iterovat. Tím ušetřím spoustu času a soustředím se na tvorbu místo konstantního ladění stylů.',
-    },
-    {
-      id: 5,
-      heading: 'JavaScript',
-      text: 'JavaScript je mým společníkem pro interaktivitu na webu. Mám dobrý přehled o základech a umím efektivně pracovat s funkcemi, událostmi a DOMem. Nicméně, v poslední době jsem propadl kouzlu Reactu, který umožňuje ještě sofistikovanější interaktivní weby.',
-    },
-    {
-        id: 6,
-        heading: 'React',
-        text: 'React je pro mě jako nekonečný zdroj inspirace. Mým cílem je stále se zlepšovat a zdokonalovat ve vytváření moderních a interaktivních webů. V mé práci s Reactem využívám široké spektrum nástrojů a knihoven. Často pracuji s komponentami, kde využívám useState a useEffect pro dynamické změny stavu a efektivní práci s daty. React Router Dom mi umožňuje vytvářet plynulé a uživatelsky přívětivé navigace mezi stránkami. Pro vizuální efekty využívám react-icons a pro pohledové záležitosti react-motion, což přidává plynulé animace a moderní vzhled.',
-      },
-      
-      
-    {
-      id: 7,
-      heading: 'Firebase',
-      text: 'Zkušenosti s Firebase mi umožňují efektivně pracovat s databázemi a autentizací. I když s ním zatím nemám tak hluboké zkušenosti jako s některými dalšími technologiemi, umím s ním pracovat a vytvořit jednoduché interaktivní aplikace.',
-    },
-    {
-      id: 8,
-      heading: 'Git',
-      text: 'Git je mým věrným společníkem v každodenní práci. Denně ho používám k udržování a verzování mého kódu. Zvládám práci s větvemi (branches), posíláním verzí (commity) celkem i slučováním změn (merge) pro efektivní spolupráci na projektech.',
-    },
-    {
-      id: 9,
-      heading: 'Ostatní',
-      text: 'Mám také zkušenosti s ovládáním příkazového řádku pomocí Git Bash a Git Kraken. V oblasti stylování mám základní schopnosti práce s grafickým programem Canva, kde dokáži vytvářet jednoduché a esteticky působivé designy.',
-    },
-  
-  ];
-  
-  export default dataExperience;
-  
+  {
+    id: 1,
+    heading: "HTML",
+    text: "Základní stavební kámen webu. Umím vytvářet strukturu obsahu, pracovat s tagy a elementy a zajistit, aby byla stránka dobře strukturovaná a přístupná.",
+  },
+  {
+    id: 2,
+    heading: "CSS",
+    text: "Kouzlo vizuálního vzhledu stránky. Nebojím se experimentovat s různými styly, od responzivního flexboxu až po CSS Grid, ale mám slabost pro flexbox.",
+  },
+  {
+    id: 3,
+    heading: "SCSS",
+    text: "SCSS mi umožňuje psát čistý a strukturovaný kód díky možnosti dědičnosti a mixinů. Rád využívám jeho sílu k vytvoření efektivních a dobře organizovaných stylů, a také se s ním lépe orientuji v rozsáhlejších projektech.",
+  },
+  {
+    id: 4,
+    heading: "Tailwind CSS",
+    text: "Mým nejoblíbenějším stylem je Tailwind CSS. Jeho jedinečné třídy mi umožňují rychle a elegantně implementovat designové prvky a rychle iterovat. Tím ušetřím spoustu času a soustředím se na tvorbu místo konstantního ladění stylů.",
+  },
+  {
+    id: 5,
+    heading: "JavaScript",
+    text: "JavaScript je mým společníkem pro interaktivitu na webu. Mám dobrý přehled o základech a umím efektivně pracovat s funkcemi, událostmi a DOMem. Nicméně, v poslední době jsem propadl kouzlu Reactu, který umožňuje ještě sofistikovanější interaktivní weby.",
+  },
+  {
+    id: 6,
+    heading: "TypeScript",
+    text: "Mám povědomí o konceptech statického typování v TypeScriptu, a již jsem ho úspěšně použil v několika projektech. Teď se snažím dále zlepšovat svoje dovednosti v tomto jazyce a posunout svůj vývoj na vyšší úroveň. Jsem přesvědčen, že učení TypeScriptu je pro mě přijatelné, protože jsem již zvládl JavaScript, na němž TypeScript stojí.",
+  },
+  {
+    id: 7,
+    heading: "React",
+    text: "React je pro mě jako nekonečný zdroj inspirace. Mým cílem je stále se zlepšovat a zdokonalovat ve vytváření moderních a interaktivních webů. V mé práci s Reactem využívám široké spektrum nástrojů a knihoven. Často pracuji s komponentami, kde využívám useState a useEffect pro dynamické změny stavu a efektivní práci s daty. React Router Dom mi umožňuje vytvářet plynulé a uživatelsky přívětivé navigace mezi stránkami. Pro vizuální efekty využívám react-icons a pro pohledové záležitosti react-motion, což přidává plynulé animace a moderní vzhled.",
+  },
+
+  {
+    id: 8,
+    heading: "Firebase",
+    text: "Zkušenosti s Firebase mi umožňují efektivně pracovat s databázemi a autentizací. I když s ním zatím nemám tak hluboké zkušenosti jako s některými dalšími technologiemi, umím s ním pracovat a vytvořit jednoduché interaktivní aplikace.",
+  },
+  {
+    id: 9,
+    heading: "Git",
+    text: "Git je mým věrným společníkem v každodenní práci. Denně ho používám k udržování a verzování mého kódu. Zvládám práci s větvemi (branches), posíláním verzí (commity) celkem i slučováním změn (merge) pro efektivní spolupráci na projektech.",
+  },
+  {
+    id: 10,
+    heading: "Ostatní",
+    text: "Mám také zkušenosti s ovládáním příkazového řádku pomocí Git Bash a Git Kraken. V oblasti stylování mám základní schopnosti práce s grafickým programem Canva, kde dokáži vytvářet jednoduché a esteticky působivé designy.",
+  },
+];
+
+export default dataExperience;
